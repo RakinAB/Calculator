72da258db7c0a7e247fbaab93db38cecaabaa517
RakinAB <142949864+RakinAB@users.noreply.github.com>
2023-09-23 15:42:40 -0500

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,16 @@
+package lab10;
+
+
+import javax.swing.*;
+
+import java.awt.*;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		GUI gui = new GUI();
+		gui.start();
+	}
+
+}
+
